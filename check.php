eb23fbe1049eba39994dcc0426a57b8eeaea0daa
naumura <yutaro0616@gmail.com>
2020-10-06 20:51:13 +0900

    add_phpkiso_complete

---
+++ check.php
@@ -0,0 +1,57 @@
+<?php
+  
+  $nickname = htmlspecialchars($_POST['nickname']);
+  $email = htmlspecialchars($_POST['email']);
+  $content = htmlspecialchars($_POST['content']);
+
+  // サニタイジングなし
+  // $nickname = $_POST['nickname'];
+  // $email = $_POST['email'];
+  // $content = $_POST['content'];
+
+  // ニックネーム
+  if ($nickname == '') {
+    $nickname_result = 'ニックネームが入力されていません。';
+  } else {
+    $nickname_result = 'ようこそ' . $nickname .'様';
+  }
+  // メールアドレス
+  if ($email == '') {
+    $email_result = 'メールアドレスが入力されていません。';
+  } else {
+    $email_result = 'メールアドレス：' . $email;
+  }
+  // お問い合わせ内容
+  if ($content == '') {
+    $content_result = 'お問い合わせ内容が入力されていません。';
+  } else {
+    $content_result = 'お問い合わせ内容：' . $content;
+  }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>入力内容確認</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1>入力内容確認</h1>
+  <p><?php echo $nickname_result; ?></p>
+  <p><?php echo $email_result; ?></p>
+  <p><?php echo $content_result; ?></p>
+  <form method="post" action="thanks.php">
+  <!-- GET送信の場合
+  <form method="get" action="thanks.php"> -->
+      <input type="hidden" name="nickname" value="<?php echo $nickname; ?>">
+      <input type="hidden" name="email" value="<?php echo $email; ?>">
+      <input type="hidden" name="content" value="<?php echo $content; ?>">
+
+      <input type="button" onclick="history.back()" value="戻る">
+      <?php if ($nickname != '' && $email != '' && $content != ''): ?>
+        <input type="submit" value="OK">
+      <?php endif; ?>
+
+  <!-- <a href="index.html">戻る</a> -->
+  </form>
+</body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,42 @@
+<?php
+  $nickname = htmlspecialchars($_POST['nickname']);
+  $email = htmlspecialchars($_POST['email']);
+  $content = htmlspecialchars($_POST['content']);
+
+  // check.phpからthanks.phpへGET送信でおくる場合
+  // $nickname = htmlspecialchars($_GET['nickname']);
+  // $email = htmlspecialchars($_GET['email']);
+  // $content = htmlspecialchars($_GET['content']);
+
+  // １．データベースに接続する
+  $dsn = 'mysql:dbname=phpkiso;host=localhost';
+  $user = 'root';
+  $password='';
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8');
+
+  // ２．SQL文を実行する
+  $sql = 'INSERT INTO `survey`(`nickname`, `email`, `content`) VALUES ("'. $nickname.'", "'.$email.'", "'.$content.'")';
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+
+  // ３．データベースを切断する
+  $dbh = null;
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>送信完了</title>
+</head>
+<body>
+  <h1>お問い合わせありがとうございました！</h1>
+  <div>
+    <h3>お問い合わせ詳細内容</h3>
+    <p>ニックネーム：<?php echo $nickname; ?></p>
+    <p>メールアドレス：<?php echo $email; ?></p>
+    <p>お問い合わせ内容：<?php echo $content; ?></p>
+  </div>
+</body>
+</html>
